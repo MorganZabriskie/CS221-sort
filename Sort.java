ec3432a02f4a027cabb4213fbb2270b2c4d22010
Morgan Zabriskie <morganzabriskie@u.boisestate.edu>
2024-11-26 17:27:51 -0700

    converting to IU double linked list

--- Sort.java
+++ Sort.java
@@ -19,7 +19,7 @@ public class Sort
 	 */
 	private static <T> IndexedUnsortedList<T> newList() 
 	{
-		return new WrappedDLL<T>(); //TODO: replace with your IUDoubleLinkedList for extra-credit
+		return new IUDoubleLinkedList<T>();
 	}
 	
 	/**
@@ -69,49 +69,41 @@ public class Sort
 	 */
 	private static <T extends Comparable<T>> void mergesort(IndexedUnsortedList<T> list)
 	{
-		// TODO: Implement recursive mergesort algorithm 
 		// split list into right and left halves
 		if(list.size() > 1) {
 			int mid = list.size() / 2;
-			WrappedDLL<T> leftList = new WrappedDLL<T>();
-			WrappedDLL<T> rightList = new WrappedDLL<T>();
+			IUDoubleLinkedList<T> leftList = new IUDoubleLinkedList<T>();
+			IUDoubleLinkedList<T> rightList = new IUDoubleLinkedList<T>();
 
 			// add first half to leftList
 			for(int i = 0; i < mid; i++) {
-				leftList.add(i, list.get(i));
-				list.remove(i);
+				leftList.addToRear(list.removeFirst());
 			}
 			mergesort(leftList);
 
 			// add second half to rightList
 			for(int i = 0; i < list.size(); i++) {
-				rightList.add(i, list.get(i));
-				list.remove(i);
+				rightList.addToRear(list.removeFirst());
 			}
 			mergesort(rightList);
 
 			// sort two lists back into main list
 			while(!leftList.isEmpty() && !rightList.isEmpty()) {
 				if(rightList.get(0).compareTo(leftList.get(0)) < 0) {
-					list.addToRear(rightList.get(0));
-					rightList.remove(0);
+					list.addToRear(rightList.removeFirst());
 				} else if (rightList.get(0).compareTo(leftList.get(0)) > 0) {
-					list.addToRear(leftList.get(0));
-					leftList.remove(0);
+					list.addToRear(leftList.removeFirst());
 				} else {
-					list.addToRear(rightList.get(0));
-					rightList.remove(0);
+					list.addToRear(rightList.removeFirst());
 				}
 			}
 
 			if(!rightList.isEmpty()) {
-				list.addToRear(rightList.get(0));
-				rightList.remove(0);
+				list.addToRear(rightList.removeFirst());
 			}
 
 			if (!leftList.isEmpty()) {
-				list.addToRear(leftList.get(0));
-				leftList.remove(0);
+				list.addToRear(leftList.removeFirst());
 			}
 		} 
 	}
@@ -131,48 +123,40 @@ public class Sort
 	 */
 	private static <T> void mergesort(IndexedUnsortedList<T> list, Comparator<T> c)
 	{
-		// TODO: Implement recursive mergesort algorithm using Comparator
 		if(list.size() > 1) {
 			int mid = list.size() / 2;
-			WrappedDLL<T> leftList = new WrappedDLL<T>();
-			WrappedDLL<T> rightList = new WrappedDLL<T>();
+			IUDoubleLinkedList<T> leftList = new IUDoubleLinkedList<T>();
+			IUDoubleLinkedList<T> rightList = new IUDoubleLinkedList<T>();
 
 			// add first half to leftList
-			for(int i = 0; i < mid; i++) {
-				leftList.add(i, list.get(i));
-				list.remove(i);
+			for (int i = 0; i < mid; i++) {
+				leftList.addToRear(list.removeFirst());
 			}
 			mergesort(leftList, c);
 
 			// add second half to rightList
-			for(int i = 0; i < list.size(); i++) {
-				rightList.add(i, list.get(i));
-				list.remove(i);
+			for (int i = 0; i < list.size(); i++) {
+				rightList.addToRear(list.removeFirst());
 			}
 			mergesort(rightList, c);
 
 			// sort two lists back into main list
 			while(!leftList.isEmpty() && !rightList.isEmpty()) {
 				if(c.compare(rightList.get(0), leftList.get(0)) < 0) {
-					list.addToRear(rightList.get(0));
-					rightList.remove(0);
+					list.addToRear(rightList.removeFirst());
 				} else if (c.compare(rightList.get(0), leftList.get(0)) > 0) {
-					list.addToRear(leftList.get(0));
-					leftList.remove(0);
+					list.addToRear(leftList.removeFirst());
 				} else {
-					list.addToRear(rightList.get(0));
-					rightList.remove(0);
+					list.addToRear(rightList.removeFirst());
 				}
 			}
 
 			if(!rightList.isEmpty()) {
-				list.addToRear(rightList.get(0));
-				rightList.remove(0);
+				list.addToRear(rightList.removeFirst());
 			}
 
 			if (!leftList.isEmpty()) {
-				list.addToRear(leftList.get(0));
-				leftList.remove(0);
+				list.addToRear(leftList.removeFirst());
 			}
 		} 
 	}
